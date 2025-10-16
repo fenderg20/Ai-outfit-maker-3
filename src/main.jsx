e7ac449f3da6066ceaa6fb49f0df32c1f52879d1
fenderg20 <50307071+fenderg20@users.noreply.github.com>
2025-10-16 20:26:29 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import OutfitMaker from "./OutfitMaker.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <OutfitMaker />
+  </React.StrictMode>
+);
